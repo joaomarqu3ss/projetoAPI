dd9b733e3b4099898b71dba18befe4b51d6f0d27
joaomarqu3ss <jpedrogamer92@gmail.com>
2025-05-18 15:07:23 -0300

    Projeto Api

---
+++ src/main/java/br/com/cotiinformatica/repositories/TarefaRepository.java
@@ -0,0 +1,11 @@
+package br.com.cotiinformatica.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.cotiinformatica.entities.Tarefa;
+
+public interface TarefaRepository extends JpaRepository<Tarefa, UUID> {
+
+}
